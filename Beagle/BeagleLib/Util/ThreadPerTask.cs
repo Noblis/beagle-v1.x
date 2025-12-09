ea6c7e665f8e4cf46c7b1431a88fd1fbac3e61a3
Ilya Basin (LG) <ilya.basin@gmail.com>
2025-12-09 17:01:47 -0500

    progress

---
+++ Beagle/BeagleLib/Util/ThreadPerTask.cs
@@ -0,0 +1,15 @@
+﻿public class ThreadPerTask : TaskScheduler
+{
+    protected override void QueueTask(Task task)
+    {
+        new Thread(() => this.TryExecuteTask(task))
+        {
+            IsBackground = true
+        }.Start();
+    }
+
+    protected override bool TryExecuteTaskInline(Task task,
+        bool taskWasPreviouslyQueued) => false;
+
+    protected override IEnumerable<Task> GetScheduledTasks() { yield break; }
+}
