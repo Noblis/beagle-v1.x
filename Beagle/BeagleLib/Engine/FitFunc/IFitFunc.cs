e4e4b5338106e18e73238efd5ed371c56df4bfd9
Ilya Basin (LG) <ilya.basin@gmail.com>
2025-12-09 10:03:39 -0500

    Added correclation flag to IFitFunc

--- Beagle/BeagleLib/Engine/FitFunc/IFitFunc.cs
+++ Beagle/BeagleLib/Engine/FitFunc/IFitFunc.cs
@@ -4,6 +4,7 @@ namespace BeagleLib.Engine.FitFunc;
 
 public interface IFitFunc
 {
+    bool UseHardcodedCorrelationFit { get; }
     int FitFunction(ArrayView<float> arrayViewInputs, uint startIdx, uint length, float output, float correctOutput);
     int FitFunction(float output, float correctOutput);
     int FitFunctionIfInvalid(bool isOutputValid, bool isCorrectOutputValid);
