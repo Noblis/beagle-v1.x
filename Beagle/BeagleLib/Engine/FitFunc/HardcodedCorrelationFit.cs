9130dc851dcb5d957e015d8dc144cd4817b7961c
Ilya Basin (LG) <ilya.basin@gmail.com>
2025-12-09 12:10:20 -0500

    progress

--- Beagle/BeagleLib/Engine/FitFunc/HardcodedCorrelationFit.cs
+++ Beagle/BeagleLib/Engine/FitFunc/HardcodedCorrelationFit.cs
@@ -1,4 +1,5 @@
-﻿using ILGPU;
+﻿using System.Diagnostics;
+using ILGPU;
 
 namespace BeagleLib.Engine.FitFunc;
 
@@ -8,16 +9,19 @@ public struct HardcodedCorrelationFit : IFitFunc
 
     public int FitFunction(ArrayView<float> arrayViewInputs, uint startIdx, uint length, float output, float correctOutput)
     {
-        throw new InvalidOperationException();
+        Debug.Assert(false, "HardcodedCorrelationFit.FitFunction(ArrayView<float> arrayViewInputs, uint startIdx, uint length, float output, float correctOutput) call");
+        return 0;
     }
 
     public int FitFunction(float output, float correctOutput)
     {
-        throw new InvalidOperationException();
+        Debug.Assert(false, "HardcodedCorrelationFit.FitFunction (float output, float correctOutput) call");
+        return 0;
     }
 
     public int FitFunctionIfInvalid(bool isOutputValid, bool isCorrectOutputValid)
     {
-        throw new InvalidOperationException();
+        Debug.Assert(false, "HardcodedCorrelationFit.FitFunction(bool isOutputValid, bool isCorrectOutputValid) call");
+        return 0;
     }
 }
