fd81dc1aa28f02b97527ad0dc137a12a52899717
Ilya @ Kili <ilya.basin@gmail.com>
2026-02-13 13:20:23 -0500

    progress

--- Beagle/Run/batch.cmd
+++ Beagle/Run/batch.cmd
@@ -1,22 +1,24 @@
+set %%r = 25
+set %%s = 10
 dotnet build -c release
 cd bin\Release\net10.0
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=5 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=7 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=14 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=18 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=20 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=21 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=29 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=31 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=38 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=43 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=44 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=56 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=57 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=72 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=80 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=86 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=87 StopAfterMin=10 NoEscMenu
-rem for /l %%i in (1, 1, 10) do Run.exe RunFeynman=90 StopAfterMin=10 NoEscMenu
-for /l %%i in (1, 1, 10) do Run.exe RunFeynman=91 StopAfterMin=10 NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=5 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=7 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=14 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=18 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=20 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=21 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=29 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=31 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=38 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=43 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=44 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=56 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=57 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=72 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=80 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=86 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=87 StopAfterMin=%%s NoEscMenu
+rem for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=90 StopAfterMin=%%s NoEscMenu
+for /l %%i in (1, 1, %%r) do Run.exe RunFeynman=91 StopAfterMin=%%s NoEscMenu
 pause
