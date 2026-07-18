fe8cb50615bc3006bfc3a79264197e72123a3463
Ilya @ Kili <ilya.basin@gmail.com>
2026-07-17 20:30:54 -0400

    progress

--- Beagle/Utils/RunFullFeynman100Suite/Program.cs
+++ Beagle/Utils/RunFullFeynman100Suite/Program.cs
@@ -5,13 +5,13 @@ namespace RunFullFeynman100Suite;
 
 public static class Program
 {
-    static void Main()
-    {
-        const int stopAfterMin = 10;
-        const int feynmanEqCount = 100;
-        const int numberOfRunsPerEq = 10;
-        const bool stopAfterTypicalAchievedStraight = true;
+    private const int StopAfterMin = 10;
+    private const int FeynmanEqCount = 100;
+    private const int NumberOfRunsPerEq = 10;
+    private const bool StopAfterTypicalAchievedStraight = true;
 
+    private static void Main()
+    {
         //const int stopAfterMin = 1;
         //const int feynmanEqCount = 3;
         //const int numberOfRunsPerEq = 3;
@@ -20,8 +20,8 @@ public static class Program
 
         DateTime now = DateTime.Now;
         
-        var equationsValidatedCount = new int[feynmanEqCount];
-        var equationsRanCount = new int[feynmanEqCount];
+        var equationsValidatedCount = new int[FeynmanEqCount];
+        var equationsRanCount = new int[FeynmanEqCount];
         Environment.CurrentDirectory = "..\\..\\..\\..\\..\\Run";
         if (Directory.Exists("AppOutput")) Directory.Delete("AppOutput", true);
 
@@ -34,10 +34,10 @@ public static class Program
 
         try
         {
-            for (var eq = 1; eq <= feynmanEqCount; eq++)
+            for (var eq = 1; eq <= FeynmanEqCount; eq++)
             {
-                startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={stopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
-                for (var i = 1; i <= numberOfRunsPerEq; i++)
+                startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={StopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
+                for (var i = 1; i <= NumberOfRunsPerEq; i++)
                 {
                     using (var process = Process.Start(startInfo) ?? throw new Exception())
                     {
@@ -49,11 +49,11 @@ public static class Program
                         {
                             equationsValidatedCount[eq - 1]++;
                             // ReSharper disable RedundantLogicalConditionalExpressionOperand
-                            if (equationsValidatedCount[eq - 1] >= MathF.Ceiling(numberOfRunsPerEq / 2f) &&
+                            if (equationsValidatedCount[eq - 1] >= MathF.Ceiling(NumberOfRunsPerEq / 2f) &&
                                 equationsValidatedCount[eq - 1] == equationsRanCount[eq - 1] && 
-                                stopAfterTypicalAchievedStraight)
+                                StopAfterTypicalAchievedStraight)
                             {
-                                GenerateAndDisplayResults(equationsValidatedCount, equationsRanCount, numberOfRunsPerEq);
+                                GenerateAndDisplayResults(equationsValidatedCount, equationsRanCount);
                                 Console.WriteLine($"Typical is achieved straight ({equationsValidatedCount[eq - 1]}/{equationsRanCount[eq - 1]}), skipping the remaining runs...");
                                 break;
                             }
@@ -65,7 +65,7 @@ public static class Program
                             Environment.Exit(exitCode);
                         }
                         
-                        GenerateAndDisplayResults(equationsValidatedCount, equationsRanCount, numberOfRunsPerEq);
+                        GenerateAndDisplayResults(equationsValidatedCount, equationsRanCount);
                     }
                 }
             }
@@ -78,17 +78,17 @@ public static class Program
         Directory.Move("AppOutput", $"FeynmanAppOutput_{now.Year}-{now.Month:D2}-{now.Day:D2}-{now.Hour:D2}-{now.Minute:D2}-{now.Second:D2}");
     }
 
-    private static void GenerateAndDisplayResults(int[] equationsValidatedCount, int[] equationsRanCount, int numberOfRunsPerEq)
+    private static void GenerateAndDisplayResults(int[] equationsValidatedCount, int[] equationsRanCount)
     {
         var resultsSb = new StringBuilder();
-        for (var eqi = 1; eqi <= numberOfRunsPerEq; eqi++)
+        for (var eqi = 1; eqi <= FeynmanEqCount; eqi++)
         {
             resultsSb.AppendLine($"Equation {eqi}: {equationsValidatedCount[eqi-1]}/{equationsRanCount[eqi - 1]}");
         }
 
         var eqRan = equationsRanCount.Count(x => x > 0);
-        resultsSb.AppendLine("------------------------------------------------------------------");
-        resultsSb.AppendLine($"Typical Solved: {equationsValidatedCount.Count(x => x >= MathF.Ceiling(numberOfRunsPerEq/2f))}/{eqRan}");
+        resultsSb.AppendLine("----------------------");
+        resultsSb.AppendLine($"Typical Solved: {equationsValidatedCount.Count(x => x >= MathF.Ceiling(NumberOfRunsPerEq/2f))}/{eqRan}");
         resultsSb.AppendLine($"Best Solved: {equationsValidatedCount.Count(x => x > 0)}/{eqRan}");
 
         var results = resultsSb.ToString();
