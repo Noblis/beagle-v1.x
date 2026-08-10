1ddfe8600eb7a69c0c5f958de277f512f121e5bd
Nathan Haut <nathaniel.haut@noblis.org>
2026-08-10 10:51:41 -0400

    Replaced repeated Cos with correct operators

--- Beagle/BeagleLib/Agent/Organism.cs
+++ Beagle/BeagleLib/Agent/Organism.cs
@@ -253,32 +253,32 @@ public class Organism
                 }
                 case "TAN":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Tan));
                     break;
                 }
                 case "ARCOS":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Arcos));
                     break;
                 }
                 case "ARCSIN":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Arcsin));
                     break;
                 }
                 case "ARCTAN":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Arctan));
                     break;
                 }
                 case "TANH":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Tanh));
                     break;
                 }
                 case "EXP":
                 {
-                    commands.Add(new Command(OpEnum.Cos));
+                    commands.Add(new Command(OpEnum.Exp));
                     break;
                 }
                 case "POW":
