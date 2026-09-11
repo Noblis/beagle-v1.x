93c2e823cf99714cdc0d208c77490fcca4dd6cb1
Ilya @ Kili <ilya.basin@gmail.com>
2026-09-11 15:11:46 -0400

    progress

--- Beagle/Utils/RunFullFeynman100Suite/Program.cs
+++ Beagle/Utils/RunFullFeynman100Suite/Program.cs
@@ -5,14 +5,17 @@ namespace RunFullFeynman100Suite;
 
 public static class Program
 {
+    public enum ProblemType
+    {
+        Easy, Medium, Hard 
+    }
+    
     private const string RelativePathToRunProject = "../../../../../Run"; //if running from Visual Studio or Rider
     //private const string RelativePathToRunProject = "../../Run"; //if running from command line
 
-    private const int StopAfterMin = 10; //-1;
-    private const long StopAfterBirths = -1; //350_000_000;
-
-    private const int ExpressStopAfterMin = -1; //4;
-    private const long ExpressStopAfterBirths = 135_000_000;
+    private const int HardStopAfterMin = 10; 
+    private const int MediumStopAfterMin = 4;
+    private const int EasyStopAfterMin = 1;
 
     private const int FeynmanEqCount = 100;
     private const int NumberOfRunsPerEq = 10;
@@ -51,16 +54,21 @@ public static class Program
         {
             for (var eq = 1; eq <= FeynmanEqCount; eq++)
             {
-                bool runningExpress;
+                ProblemType runningAs;
                 if (HardProblems.Contains(eq))
                 {
-                    runningExpress = false;
-                    startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={StopAfterMin} StopAfterBirths={StopAfterBirths} RunFeynman={eq} NoEscMenu #useLibDevice";
+                    runningAs = ProblemType.Hard;
+                    startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={HardStopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
+                }
+                else if (EasyProblems.Contains(eq))
+                {
+                    runningAs = ProblemType.Easy;
+                    startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={EasyStopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
                 }
                 else
                 {
-                    runningExpress = true;
-                    startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={ExpressStopAfterMin} StopAfterBirths={ExpressStopAfterBirths} RunFeynman={eq} NoEscMenu #useLibDevice";
+                    runningAs = ProblemType.Medium;
+                    startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={MediumStopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
                 }
 
                 for (var i = 1; i <= NumberOfRunsPerEq; i++)
@@ -90,16 +98,23 @@ public static class Program
                         }
                         else if (exitCode == 1)
                         {
-                            if (runningExpress)
+                            if (runningAs == ProblemType.Easy)
                             {
-                                runningExpress = false;
+                                runningAs = ProblemType.Medium;
                                 equationsRanCount[eq - 1] = equationsValidatedCount[eq - 1] = 0;
-                                startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={StopAfterMin} StopAfterBirths={StopAfterBirths} RunFeynman={eq} NoEscMenu #useLibDevice";
+                                startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={MediumStopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
+                                i = 0;
+                            }
+                            else if (runningAs == ProblemType.Medium)
+                            {
+                                runningAs = ProblemType.Hard;
+                                equationsRanCount[eq - 1] = equationsValidatedCount[eq - 1] = 0;
+                                startInfo.Arguments = $"run --configuration Release --no-launch-profile -- StopAfterMin={HardStopAfterMin} RunFeynman={eq} NoEscMenu #useLibDevice";
                                 i = 0;
                             }
                             else
                             {
-                                //if no hope for typical but we already have best
+                                //if running as hard no hope for typical but we already have best
                                 if (equationsValidatedCount[eq - 1] > 0 &&
                                     NumberOfRunsPerEq - equationsRanCount[eq - 1] <
                                     MathF.Ceiling(NumberOfRunsPerEq / 2f) - equationsValidatedCount[eq - 1])
